697aa577dec37f7bccd88bfe277a6b73c46845f5
Mohannad Alastal <90284916+MhndAs@users.noreply.github.com>
2025-09-01 18:56:58 +0300

    Fix CORS Package

--- awn/settings.py
+++ awn/settings.py
@@ -185,8 +185,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://preview--chime-comms.lovable.app",
     "https://chime-comms.lovable.app",
     "https://lovable.app",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
     "https://awn-three.vercel.app",
 ]
 
